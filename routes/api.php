f77f5e796beaae62725ccbbc47375a549de8b811
Robert Yepez <robertyepez0208@hotmail.com>
2025-10-23 17:44:57 +0200

    feat: Add new API route for user registration

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\TokenController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,3 +11,9 @@ Route::get('/user', function (Request $request) {
 
 //request access token
 Route::post('/token', [TokenController::class, 'token'])->name('token');
+
+// search artists,tacks and albums
+Route::controller(SearchController::class)
+::class)->group(function () {
+    Route::get('/search', 'search')->name('search');
+});
