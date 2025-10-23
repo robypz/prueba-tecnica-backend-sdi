9708751585e0619aba3abf27d5e0e2b434e89049
Robert Yepez <robertyepez0208@hotmail.com>
2025-10-23 17:42:08 +0200

    feat: Add token generation route and update .gitignore

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\TokenController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//request access token
+Route::post('/token', [TokenController::class, 'token'])->name('token');
